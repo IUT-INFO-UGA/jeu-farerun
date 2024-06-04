64fb57de07917a01b392f61b4ae8411d601e02c8
Énée DI IORIO <enee.prog@gmail.com>
2024-06-04 14:44:41 +0200

    add test configuration

--- test/GuerrierTest.java
+++ test/GuerrierTest.java
@@ -1,29 +1,43 @@
 package jeu.test;
 
+import jeu.Guerrier;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class GuerrierTest {
 
 	@org.junit.jupiter.api.Test
 	void getForce() {
+		final Guerrier guerrier = new Guerrier();
+		assertEquals(10, guerrier.getForce(), "La force du guerrier doit être de 10");
 	}
 
 	@org.junit.jupiter.api.Test
 	void getPointDeVie() {
+		final Guerrier guerrier = new Guerrier();
+		assertEquals(100, guerrier.getPointDeVie(), "Les points de vie du guerrier doivent être de 100");
 	}
 
 	@org.junit.jupiter.api.Test
 	void setPointDeVie() {
+		final Guerrier guerrier = new Guerrier();
+		guerrier.setPointDeVie(50);
+		assertEquals(50, guerrier.getPointDeVie(), "Les points de vie du guerrier doivent être de 50");
 	}
 
 	@org.junit.jupiter.api.Test
 	void estVivant() {
+		final Guerrier guerrier = new Guerrier();
+		assertTrue(guerrier.estVivant(), "Le guerrier doit être vivant");
+		guerrier.setPointDeVie(0);
+		assertFalse(guerrier.estVivant(), "Le guerrier doit être mort");
 	}
 
 	@org.junit.jupiter.api.Test
 	void attaquer() {
-	}
-
-	@org.junit.jupiter.api.Test
-	void subirDegats() {
+		final Guerrier guerrier = new Guerrier();
+		final Guerrier guerrier2 = new Guerrier();
+		guerrier.attaquer(guerrier2);
+		assertNotEquals(100, guerrier2.getPointDeVie(), "Les points de vie du guerrier2 doivent être de 90");
 	}
 }
