496b8a5110bd2815cc5e8e8134c7a01c31acb486
Énée DI IORIO <enee.prog@gmail.com>
2024-06-04 14:49:15 +0200

    add chefElfTest

---
+++ test/ChefElfeTest.java
@@ -0,0 +1,12 @@
+package jeu.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChefElfeTest {
+
+	@Test
+	void getForce() {
+	}
+}
---
+++ test/ChefNainTest.java
@@ -0,0 +1,12 @@
+package jeu.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChefNainTest {
+
+	@Test
+	void subirDegats() {
+	}
+}
