5381fdbba006c8a07590cc900f84f6d578737b86
Énée DI IORIO <enee.prog@gmail.com>
2024-06-04 14:44:22 +0200

    add test for guerrier

---
+++ test/GuerrierTest.java
@@ -0,0 +1,29 @@
+package jeu.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class GuerrierTest {
+
+	@org.junit.jupiter.api.Test
+	void getForce() {
+	}
+
+	@org.junit.jupiter.api.Test
+	void getPointDeVie() {
+	}
+
+	@org.junit.jupiter.api.Test
+	void setPointDeVie() {
+	}
+
+	@org.junit.jupiter.api.Test
+	void estVivant() {
+	}
+
+	@org.junit.jupiter.api.Test
+	void attaquer() {
+	}
+
+	@org.junit.jupiter.api.Test
+	void subirDegats() {
+	}
+}
